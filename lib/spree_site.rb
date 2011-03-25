1f74e60cbf6edd92726a8a891eb34060869252ec
Richard Viney <richard.viney@gmail.com>
2011-03-25 17:03:57 +1300

    Added workaround for Spree bug that causes rake tasks to be run twice

--- lib/spree_site.rb
+++ lib/spree_site.rb
@@ -4,5 +4,15 @@ module SpreeSite
       # Add your custom site logic here
     end
     config.to_prepare &method(:activate).to_proc
+    
+    #
+    # Spree v0.50.0 causes application rake tasks to be run twice, a fix has been committed but has not yet been
+    # officially released, so backport for now. This can be removed when next upgrading Spree.
+    #
+    #   http://railsdog.lighthouseapp.com/projects/31096/tickets/1792-spree-causes-rake-tasks-to-be-invoked-twice
+    #   https://github.com/spree/spree/commit/fb5490157dfe3cd4d7da6e20a0fe61065b097639
+    #
+    def load_tasks
+    end
   end
 end
