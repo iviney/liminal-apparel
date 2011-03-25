933e85741c3efa406fa434d662fda7d09bf76745
Ian Viney <ian@viney.net.nz>
2011-03-25 13:18:31 +1300

    Blank Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LiminalApparel::Application.config.secret_token = 'd9344b96241c03d94132249f9993c81e8a14099074621ae4696bacffe0b0b08b4a68930fcfb177f41508a921b988609247e48febf5f33a0bea6907cedafaf670'
