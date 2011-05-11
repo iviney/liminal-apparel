12e8a0534678ee6d9cb745916bea5ae8745bce20
Jonathan Viney <jonathan.viney@gmail.com>
2011-05-11 19:40:18 +1200

    Use one of three images for the main page. Issue #1.

--- public/javascripts/liminal/frontpage.js
+++ public/javascripts/liminal/frontpage.js
@@ -20,20 +20,3 @@ var add_image_handlers = function() {
 jQuery(document).ready(function() {
   add_image_handlers();
 });
-
-// The main banner cycles between several images
-// Code based on http://jquery-howto.blogspot.com/2009/05/replacing-images-at-time-intervals.html
-
-function change_image(){
-  var $active = $('#myGallery .active');
-  var $next = ($('#myGallery .active').next().length > 0) ? $('#myGallery .active').next() : $('#myGallery img:first');
-  $active.fadeOut(1000, function(){
-    $active.removeClass('active');
-  });
-  $next.fadeIn(1000).addClass('active');
-};
-
-jQuery(document).ready(function(){
-  // Run our change_image() function every 10 secs
-  setInterval('change_image()', 3000);
-});
