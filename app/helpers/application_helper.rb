5a2d916d62f664f27f7880b369bf73f1a6344396
Jonathan Viney <jonathan.viney@gmail.com>
2011-06-20 22:22:29 +1200

    Add link to 'other' site.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def link_to_other_site
+    if australia?
+      link_to "Go to New Zealand Store (NZD)", site_path("new_zealand"), :class => "other-site"
+    else
+      link_to "Go to Australian Store (AUD)", site_path("australia"), :class => "other-site"
+    end
+  end
 end
