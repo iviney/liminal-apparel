078d3996ed70278289df8b3585cae8a8e24dbec8
Richard Viney <richard.viney@gmail.com>
2011-06-20 22:43:33 +1200

    Pass current currency through to PayPal

--- app/controllers/checkout_controller_decorator.rb
+++ app/controllers/checkout_controller_decorator.rb
@@ -1,6 +1,10 @@
 CheckoutController.class_eval do
   before_filter :only_allow_active_country, :only => :update
   
+  def paypal_site_opts
+    { :currency => Site.active_currency }
+  end
+
   private
     def only_allow_active_country
       if params[:order] and attributes = params[:order][:bill_address_attributes]
