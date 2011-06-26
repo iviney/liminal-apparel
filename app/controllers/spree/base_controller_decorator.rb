a68334fa7be8a49fd3a42a11531e6bebae56a9f2
Jonathan Viney <jonathan.viney@gmail.com>
2011-06-27 09:54:53 +1200

    Do not set an active currency in the admin area.

--- app/controllers/spree/base_controller_decorator.rb
+++ app/controllers/spree/base_controller_decorator.rb
@@ -4,8 +4,12 @@ Spree::BaseController.class_eval do
   before_filter :set_currency
   
   private
-  
-  def set_currency
-    Site.active_currency = (australia? ? "AUD" : "NZD")
-  end
+    def set_currency
+      # In the admin area we do not want an active currency
+      if self.class.name.starts_with?("Admin::")
+        Site.active_currency = nil
+      else
+        Site.active_currency = (australia? ? "AUD" : "NZD")
+      end
+    end
 end
