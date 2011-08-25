721ffdbadbcd3c110f0d85b223acf7532dc4ca0b
Ian Viney <ian@viney.net.nz>
2011-08-26 10:04:08 +1200

    Initialized site_url config so that the link in the password-reset email points back to the correct domain.

--- config/initializers/configuration_defaults.rb
+++ config/initializers/configuration_defaults.rb
@@ -1,6 +1,7 @@
 # Overrides some of the Spree gem defaults in app/models/app_configuration.rb
 
 Spree::Config.set(:site_name => 'Liminal Apparel Store')
+Spree::Config.set(:site_url => 'test.store.liminal.org.nz') # TBD can perhaps use request.host ?
 Spree::Config.set :default_country_id => 145 # New Zealand (see numbers in db/default/countries.yml)
 #Spree::Config.set :admin_interface_logo=> 'admin/bg/spree_50.png' # TBD: change this
 Spree::Config.set :checkout_zone => 'Liminal Sales Zone'
