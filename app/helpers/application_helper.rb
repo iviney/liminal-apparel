be5d0ae8fb140e4d1e12c8d7a171bd919b7946bc
Ian Viney <ian@viney.net.nz>
2011-08-28 19:24:48 +1200

    Turn off shop-related stuff because it's not working yet.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,12 @@ module ApplicationHelper
     if australia?
       link_to "Go to New Zealand Store (NZD)", root_url(:host => Site.domains[:new_zealand], :port => request.port), :class => "other-site"
     else
-      link_to "Go to Australian Store (AUD)", root_url(:host => Site.domains[:australia], :port => request.port), :class => "other-site"
+      #TBD Enable this when Australian site is operational.
+      #link_to "Go to Australian Store (AUD)", root_url(:host => Site.domains[:australia], :port => request.port), :class => "other-site"
     end
+  end
+
+  def shop_enabled
+    false # setting this to false turns off the shopping aspects of the site
   end
 end
