3fd154bb6e0da8c8167aa90b591f9545c22730b9
Ian Viney <ian@viney.net.nz>
2011-09-28 09:22:55 +1300

    :-)

--- app/controllers/spree/base_controller_decorator.rb
+++ app/controllers/spree/base_controller_decorator.rb
@@ -7,7 +7,7 @@ Spree::BaseController.class_eval do
     def set_currency
       Site.active_currency = nil
       
-      # In the admin area we do not want an active currency
+      # In the admin area we do not want an active currency!
       unless self.class.name.starts_with?("Admin::")
         if new_zealand?
           Site.active_currency = "NZD" 
