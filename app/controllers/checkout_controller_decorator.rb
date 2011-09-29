d16fa3a6ec89ae70b8ba423543815b2527151d4f
Jonathan Viney <jonathan.viney@gmail.com>
2011-09-29 14:00:05 +1300

    Add Paypal logo from ChurchOffice https temporarily.

--- app/controllers/checkout_controller_decorator.rb
+++ app/controllers/checkout_controller_decorator.rb
@@ -2,7 +2,10 @@ CheckoutController.class_eval do
   before_filter :only_allow_active_country, :only => :update
   
   def paypal_site_opts
-    { :currency => @order.currency }
+    {
+      :currency => @order.currency,
+      :header_image => "https://app.churchoffice.org.nz#{Spree::Config[:logo]}"
+    }
   end
 
   private
