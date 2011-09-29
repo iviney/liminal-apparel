8455e2dc9d82eb7ecd853378b7aeae7eb4dcb3ec
Jonathan Viney <jonathan.viney@gmail.com>
2011-09-29 13:40:51 +1300

    Do not set site_url on every server launch, use the db value. Update logo path.

--- config/initializers/configuration_defaults.rb
+++ config/initializers/configuration_defaults.rb
@@ -1,9 +1,8 @@
 # Overrides some of the Spree gem defaults in app/models/app_configuration.rb
 
-Spree::Config.set(:site_name => 'Liminal Apparel Store')
-Spree::Config.set(:site_url => Rails.env.production? ? 'staging.liminal.org.nz' : 'localhost') # TBD can perhaps use request.host or SERVER_ADDRESS? But this is not run for evey request!
+Spree::Config.set :site_name => 'Liminal Apparel Store'
 Spree::Config.set :default_country_id => 145 # New Zealand (see numbers in db/default/countries.yml)
-#Spree::Config.set :admin_interface_logo=> 'admin/bg/spree_50.png' # TBD: change this
+Spree::Config.set :logo => "/images/liminal-apparel.jpg"
 Spree::Config.set :checkout_zone => 'Liminal Sales Zone'
 Spree::Config.set :required_currencies => "NZD AUD"
 Spree::Config.set :address_requires_state => false
