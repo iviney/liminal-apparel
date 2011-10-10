bc873585b931bb2e79205da71ef1ab182f34748e
Ian Viney <ian@viney.net.nz>
2011-10-10 18:07:32 +1300

    Change  Paypal tax calc so it's not dependent on the word "Tax". Reinstate flash notices during checkout. Change logging level to info so we can see what's happening Change to using spree/spree_paypal_express instead of curlyheads/spree_paypal_express.

--- Gemfile
+++ Gemfile
@@ -16,9 +16,7 @@ gem 'spree', '0.50.2'
 gem 'spree_simple_volume_pricing', :git => 'git://github.com/jviney/spree-simple-volume-pricing.git', :branch => 'master'
 
 # Adds support for the Paypal Express checkout.
-# There was a bug with the version maintained by Spree which is fixed by this fork.
-# http://groups.google.com/group/spree-user/browse_thread/thread/7a20e9c8440980f8
-gem "spree_paypal_express", :git => "git://github.com/curlyheads/spree_paypal_express.git"
+gem "spree_paypal_express", :git => "git://github.com/spree/spree_paypal_express.git"
 
 gem 'spree_additional_calculators'
 
--- app/controllers/checkout_controller_decorator.rb
+++ app/controllers/checkout_controller_decorator.rb
@@ -8,63 +8,68 @@ CheckoutController.class_eval do
     }
   end
 
-  def order_opts(order, payment_method, stage)
-    items = order.line_items.map do |item|
-      price = (item.price * 100).to_i # convert for gateway
-      { :name        => item.variant.product.name,
-        :description => item.variant.product.description[0..120],
-        :sku         => item.variant.sku,
-        :qty         => item.quantity,
-        :amount      => price,
-        :weight      => item.variant.weight,
-        :height      => item.variant.height,
-        :width       => item.variant.width,
-        :depth       => item.variant.weight }
-      end
-
-    credits = order.adjustments.map do |credit|
-      if credit.amount < 0.00
-        { :name        => credit.label,
-          :description => credit.label,
-          :sku         => credit.id,
-          :qty         => 1,
-          :amount      => (credit.amount*100).to_i }
-      end
+# ---------------------------------------------------------------------------------------------------------------------
+# This is a copy of order_opts from spree_paypal_express, with a change for tax calc (see '#OLD')
+def order_opts(order, payment_method, stage)
+  items = order.line_items.map do |item|
+    price = (item.price * 100).to_i # convert for gateway
+    { :name        => item.variant.product.name,
+      :description => (item.variant.product.description[0..120] if item.variant.product.description),
+      :sku         => item.variant.sku,
+      :quantity    => item.quantity,
+      :amount      => price,
+      :weight      => item.variant.weight,
+      :height      => item.variant.height,
+      :width       => item.variant.width,
+      :depth       => item.variant.weight }
     end
 
-    credits_total = 0
-    credits.compact!
-    if credits.present?
-      items.concat credits
-      credits_total = credits.map {|i| i[:amount] * i[:qty] }.sum
+  credits = order.adjustments.map do |credit|
+    if credit.amount < 0.00
+      { :name        => credit.label,
+        :description => credit.label,
+        :sku         => credit.id,
+        :quantity    => 1,
+        :amount      => (credit.amount*100).to_i }
     end
-
-    opts = { :return_url        => request.protocol + request.host_with_port + "/orders/#{order.number}/checkout/paypal_confirm?payment_method_id=#{payment_method}",
-             :cancel_return_url => "http://"  + request.host_with_port + "/orders/#{order.number}/edit",
-             :order_id          => order.number,
-             :custom            => order.number,
-             :items             => items,
-             :subtotal          => ((order.item_total * 100) + credits_total).to_i,
-             #OLD :tax               => ((order.adjustments.map { |a| a.amount if ( a.source_type == 'Order' && a.label == 'Tax') }.compact.sum) * 100 ).to_i,
-             :tax               => ((order.adjustments.map { |a| a.amount if ( a.source_type == 'Order' && a.originator_type == 'TaxRate') }.compact.sum) * 100).to_i,
-             :shipping          => ((order.adjustments.map { |a| a.amount if a.source_type == 'Shipment' }.compact.sum) * 100 ).to_i,
-             :money             => (order.total * 100 ).to_i }
-
-
-    if stage == "checkout"
-      opts[:handling] = 0
-
-      opts[:callback_url] = "http://"  + request.host_with_port + "/paypal_express_callbacks/#{order.number}"
-      opts[:callback_timeout] = 3
-    elsif stage == "payment"
-      #hack to add float rounding difference in as handling fee - prevents PayPal from rejecting orders
-      #because the integer totals are different from the float based total. This is temporary and will be
-      #removed once Spree's currency values are persisted as integers (normally only 1c)
-      opts[:handling] =  (order.total*100).to_i - opts.slice(:subtotal, :tax, :shipping).values.sum
-    end
-
-    opts
   end
+
+  credits_total = 0
+  credits.compact!
+  if credits.present?
+    items.concat credits
+    credits_total = credits.map {|i| i[:amount] * i[:quantity] }.sum
+  end
+
+  opts = { #:return_url        => request.protocol + request.host_with_port + "/orders/#{order.number}/checkout/paypal_confirm?payment_method_id=#{payment_method}",
+           :return_url        => "http://"  + request.host_with_port + "/orders/#{order.number}/checkout/paypal_confirm?payment_method_id=#{payment_method}",
+           :cancel_return_url => "http://"  + request.host_with_port + "/orders/#{order.number}/edit",
+           :order_id          => order.number,
+           :custom            => order.number,
+           :items             => items,
+           :subtotal          => ((order.item_total * 100) + credits_total).to_i,
+           #OLD: :tax         => ((order.adjustments.map { |a| a.amount if ( a.source_type == 'Order' && a.label == 'Tax') }.compact.sum) * 100 ).to_i,
+           :tax               => ((order.adjustments.map { |a| a.amount if ( a.source_type == 'Order' && a.originator_type == 'TaxRate') }.compact.sum) * 100).to_i,
+           :shipping          => ((order.adjustments.map { |a| a.amount if a.source_type == 'Shipment' }.compact.sum) * 100 ).to_i,
+           :money             => (order.total * 100 ).to_i }
+
+    # add correct tax amount by subtracting subtotal and shipping otherwise tax = 0 -> need to check adjustments.map
+    #OLD: opts[:tax] = (order.total*100).to_i - opts.slice(:subtotal, :shipping).values.sum
+
+  if stage == "checkout"
+    opts[:handling] = 0
+
+    opts[:callback_url] = "http://"  + request.host_with_port + "/paypal_express_callbacks/#{order.number}"
+    opts[:callback_timeout] = 3
+  elsif stage == "payment"
+    #hack to add float rounding difference in as handling fee - prevents PayPal from rejecting orders
+    #because the integer totals are different from the float based total. This is temporary and will be
+    #removed once Spree's currency values are persisted as integers (normally only 1c)
+    opts[:handling] =  (order.total*100).to_i - opts.slice(:subtotal, :tax, :shipping).values.sum
+  end
+
+  opts
+end
 
   private
     def only_allow_active_country
