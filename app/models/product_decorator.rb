cd19c8b172e567bb7b6596e108e4c2abf853498c
Jonathan Viney <jonathan.viney@gmail.com>
2011-10-29 12:36:33 +1300

    Include validations for required currencies directly on Product rather than going through the master variant.

--- app/models/product_decorator.rb
+++ app/models/product_decorator.rb
@@ -2,4 +2,7 @@ Product.class_eval do
   remove_validator :presence, :price
   
   delegate_belongs_to :master, :currency_prices
+  
+  # Just for validation of currency_prices
+  include Models::MultipleCurrencies
 end
