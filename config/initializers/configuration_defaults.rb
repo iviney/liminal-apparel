37e5147ca85888691de3c9c36cae2259cf39aa94
Ian Viney <ian@viney.net.nz>
2011-10-29 12:42:15 +1300

    Make NZD the only required currency for the moment.

--- config/initializers/configuration_defaults.rb
+++ config/initializers/configuration_defaults.rb
@@ -4,5 +4,5 @@ Spree::Config.set :site_name => 'Liminal Apparel Store'
 Spree::Config.set :default_country_id => 145 # New Zealand (see numbers in db/default/countries.yml)
 Spree::Config.set :logo => "/images/liminal-apparel.jpg"
 Spree::Config.set :checkout_zone => 'Liminal Sales Zone'
-Spree::Config.set :required_currencies => "NZD AUD"
+Spree::Config.set :required_currencies => "NZD"
 Spree::Config.set :address_requires_state => false
