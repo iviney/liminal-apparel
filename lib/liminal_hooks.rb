2f0c5ca09585057ac43f9c7d7f91a8f6f102579e
Ian Viney <ian@viney.net.nz>
2011-11-03 20:03:21 +1300

    Displayed each order's currency in list of orders.

--- lib/liminal_hooks.rb
+++ lib/liminal_hooks.rb
@@ -1,5 +1,16 @@
 class LiminalHooks < Spree::ThemeSupport::HookListener
+
   insert_before :admin_product_form_right, :partial => "admin/shared/product_currency_prices"
+
   insert_before :admin_order_show_details, :partial => "admin/orders/screen_printing"
+
+  insert_after :admin_orders_index_headers do # see spree_core-0.50.4\app\views\admin\orders\index.html.erb
+     %( <th><%= sort_link @search, :currency, t("currency") %></th>)
+  end
+
+  insert_after :admin_orders_index_rows do
+    %(<td><%= order.currency %></td>)
+  end
+
   replace :products_list_item, :partial => "shared/products_list_item"
 end
