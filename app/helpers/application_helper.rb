d59b13ad4e3e463853d95e85a6c26faa7cc691ee
Ian Viney <ian@viney.net.nz>
2011-11-03 21:25:48 +1300

    Turned on link from NZ store to Aus store.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,7 +6,7 @@ module ApplicationHelper
       link_to "Go to New Zealand Store (NZD)", root_url(:host => Site.domains[:new_zealand], :port => request.port), :class => "other-site"
     else
       #TBD Enable this when Australian site is operational.
-      #link_to "Go to Australian Store (AUD)", root_url(:host => Site.domains[:australia], :port => request.port), :class => "other-site"
+      link_to "Go to Australian Store (AUD)", root_url(:host => Site.domains[:australia], :port => request.port), :class => "other-site"
     end
   end
 
