11575afa5c132b135b556a69da53e747d8c17622
Ian Viney <ian@viney.net.nz>
2011-11-04 14:51:57 +1300

    Add extra column in admin product list to show currency prices.

--- lib/liminal_hooks.rb
+++ lib/liminal_hooks.rb
@@ -4,6 +4,7 @@ class LiminalHooks < Spree::ThemeSupport::HookListener
 
   insert_before :admin_order_show_details, :partial => "admin/orders/screen_printing"
 
+  # Add a column in admin order listing to show currency of order
   insert_after :admin_orders_index_headers do # see spree_core-0.50.4\app\views\admin\orders\index.html.erb
      %( <th><%= sort_link @search, :currency, t("currency") %></th>)
   end
@@ -12,5 +13,14 @@ class LiminalHooks < Spree::ThemeSupport::HookListener
     %(<td><%= order.currency %></td>)
   end
 
+  # Add a column in admin product listing to show currency prices
+  insert_after :admin_products_index_headers do
+    %(<th><%= sort_link @search,:currency_prices, "Currency prices" %></th>)
+  end
+
+  insert_after :admin_products_index_rows do
+    %(<td><%= product.currency_prices %></td>)
+  end
+  
   replace :products_list_item, :partial => "shared/products_list_item"
 end
