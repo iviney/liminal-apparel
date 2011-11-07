be213ce864c9ccbeee3af53213dcc781aad37a51
Ian Viney <ian@viney.net.nz>
2011-11-07 17:50:50 +1300

    Re-word Store to Site.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,10 +3,10 @@ module ApplicationHelper
   
   def link_to_other_site
     if australia?
-      link_to "Go to New Zealand Store (NZD)", root_url(:host => Site.domains[:new_zealand], :port => request.port), :class => "other-site"
+      link_to "Go to New Zealand Site (NZD)", root_url(:host => Site.domains[:new_zealand], :port => request.port), :class => "other-site"
     else
       #TBD Enable this when Australian site is operational.
-      link_to "Go to Australian Store (AUD)", root_url(:host => Site.domains[:australia], :port => request.port), :class => "other-site"
+      link_to "Go to Australian Site (AUD)", root_url(:host => Site.domains[:australia], :port => request.port), :class => "other-site"
     end
   end
 
