221952bf21d83114e1dac04470cd5b09e7b1e62d
Ian Viney <ian@viney.net.nz>
2011-11-19 22:21:52 +1300

    Show Specials image on front page instead of tweets if there are specials.

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,2 +1,9 @@
 class HomepageController < Spree::BaseController
+  def index
+    # @show_specials tells the view whether or not to show the link to specials - i.e. if there are any
+    @show_specials = false
+    if t = Taxon.find(:first,:conditions=>{:permalink=>'categories/specials'})
+      @show_specials = t.products.available.count > 0
+    end
+  end
 end
