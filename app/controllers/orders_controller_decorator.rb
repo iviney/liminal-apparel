95544b900fe230acba656c85296cf94efe634be2
Ian Viney <ian@viney.net.nz>
2011-11-21 21:06:21 +1300

    Use admin-only field to select shipping methods.

--- app/controllers/orders_controller_decorator.rb
+++ app/controllers/orders_controller_decorator.rb
@@ -1,4 +1,11 @@
 OrdersController.class_eval do
+
+  # ===================================================================================================================
+  # Redirect access to orders if the shop is closed.
+  before_filter :redirect_if_shop_closed
+
+  # ===================================================================================================================
+  # Redefine #populate
   def populate_with_screen_printing
     populate_without_screen_printing
 
@@ -11,9 +18,8 @@ OrdersController.class_eval do
     alias_method_chain :populate, :screen_printing
   end
 
-  before_filter :redirect_if_shop_closed
-
-  # Redefine show to add a check for valid parameters
+  # ===================================================================================================================
+  # Redefine #show to add a check for valid parameters
   def show
     @order = Order.find_by_number(params[:id])
     if !@order
@@ -21,6 +27,22 @@ OrdersController.class_eval do
     end
   end
 
+  # ===================================================================================================================
+  # Redefine #rate_hash to cope with admin-only shipping methods - see \spree_core-0.50.4\app\models\order.rb
+  def rate_hash
+    @rate_hash ||= available_shipping_methods(:front_end).collect do |ship_method|
+      next if ship_method.admin_only && !user.roles.any? {|r| r.name == "admin"} # ignore method if it's admin only and our user does not have an admin role
+      next unless cost = ship_method.calculator.compute(self)
+      { :id => ship_method.id,
+        :shipping_method => ship_method,
+        :name => ship_method.name,
+        :cost => cost
+      }
+    end.compact.sort_by{|r| r[:cost]}
+  end
+
+  # ===================================================================================================================
+  # Redirect access to orders if the shop is closed.
   private
 
   def redirect_if_shop_closed
--- lib/liminal_hooks.rb
+++ lib/liminal_hooks.rb
@@ -4,6 +4,7 @@ class LiminalHooks < Spree::ThemeSupport::HookListener
 
   insert_before :admin_order_show_details, :partial => "admin/orders/screen_printing"
 
+  #===================================================================================================================
   # Add a column in admin order listing to show currency of order
   insert_after :admin_orders_index_headers do # see spree_core-0.50.4\app\views\admin\orders\index.html.erb
      %( <th><%= sort_link @search, :currency, t("currency") %></th>)
@@ -13,6 +14,7 @@ class LiminalHooks < Spree::ThemeSupport::HookListener
     %(<td><%= order.currency %></td>)
   end
 
+  #===================================================================================================================
   # Add a column in admin product listing to show currency prices
   insert_after :admin_products_index_headers do
     %(<th><%= sort_link @search,:currency_prices, "Currency prices" %></th>)
@@ -24,6 +26,7 @@ class LiminalHooks < Spree::ThemeSupport::HookListener
   
   replace :products_list_item, :partial => "shared/products_list_item"
 
+  #===================================================================================================================
   # Add an admin item to maintain retailers  # see spree_core-0.50.4\app\views\admin\configurations\index.html.erb
   insert_after :admin_configurations_menu do
   <<HTML
@@ -34,24 +37,24 @@ class LiminalHooks < Spree::ThemeSupport::HookListener
 HTML
   end
 
+  #===================================================================================================================
   # Change order listing to include time on order date # see spree_core-0.50.4\app\views\admin\orders\index.html.erb
   replace :admin_orders_index_rows, :partial => 'admin/shared/orders_index_rows.html.erb'
 
+  #===================================================================================================================
   # Add an admin-only field to the shipping_methods admin forms (see spree_core-0.50.4/app/views/admin/shipping_methods/_form.html.erb)
   insert_after :admin_shipping_method_form_fields do
   <<HTML
-  <%= f.field_container :admin_only do %>
-    <label>
-    <%= f.check_box(:admin_only)  %><%= t(show_method_to_admin_users_only) %>
+  <label>
+    <%= f.check_box(:admin_only)  %><%= t(:show_method_to_admin_users_only) %>
     <br />
-    </label>
-  <% end %>
+  </label>
 HTML
   end
 
   # Add admin-only column to listing of shipping methods in admin - see \spree_core-0.50.4\app\views\admin\shipping_methods\index.html.erb
   insert_after :admin_shipping_methods_index_headers do
-    %(<th><%= t("admin_only") %></th>)
+    %(<th><%= t(:admin_only) %></th>)
   end
 
   insert_after :admin_shipping_methods_index_rows do
