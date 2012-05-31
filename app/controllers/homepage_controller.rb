3ba0307ed3bd7cc3e27a75230bc46d49026472b3
Jonathan Viney <jonathan.viney@gmail.com>
2012-05-31 17:40:41 +1200

    Force html format for homepage.

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,9 +1,17 @@
 class HomepageController < Spree::BaseController
+  before_filter :force_html_format
+
   def index
+    request.format = :html
     # @show_specials tells the view whether or not to show the link to specials - i.e. if there are any
     @show_specials = false
     if t = Taxon.find(:first,:conditions=>{:permalink=>'categories/specials'})
       @show_specials = t.products.available.count > 0
     end
   end
+
+  private
+    def force_html_format
+      request.format = :html
+    end
 end
